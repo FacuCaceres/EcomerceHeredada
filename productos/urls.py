8f82acc6b609e54950d3b156265918a283485233
FacuCaceres <facundoluiscaceres@gmail.com>
2022-06-09 12:37:02 -0300

    'CRUD'

--- productos/urls.py
+++ productos/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
 
     path('', views.productos, name = 'productos'),
     path('buscar/',views.buscar_productos , name = 'buscar_productos'),
+    path('detalle-producto/<int:id>/',views.detalle_producto , name = 'detalle-producto'),
+    path('eliminar-producto/<int:id>/',views.eliminar_producto , name = 'eliminar-producto'),
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,12 +1,11 @@
 from multiprocessing import context
+from django.http import HttpResponse
 from django.shortcuts import render
 from productos.models import Products
 # Create your views here.
 def productos(request):
     productos = Products.objects.all()
-    context = {
-        'productos':productos
-    }
+    context = { 'productos':productos  }
     return render(request,'productos/productos.html',context=context)
 
 def buscar_productos(request):
@@ -22,3 +21,35 @@ def buscar_productos(request):
         'prd_buscados' : productos
     }
     return render(request, 'productos/buscarproductos.html',context=context)
+
+    
+
+def detalle_producto(request, id):
+
+    '''EL TRY EXCEPT ES UNA VENTAJA Y DESVENTAJA PORQUE EVITA QUE TU PROGRAMA SE ROMPA PERO A LA VEZ EVITA QUE 
+    TU PROGRAMA TE AVISE CUAL ES EL ERROR EN CASO DE ROMPERSE ME PASO AHORA CON UN ERROR EN EL HTML
+    ESTABA COMENTANDO UN CODIGO Y NO DEBI COMENTAR DEBI QUITARLO ES DECIR BORRARLO'''
+
+    try:
+        producto = Products.objects.get(id=id)
+        context = {'detalle_prd':producto }
+        return render(request,'productos/detalle-producto.html',context=context)
+    except:
+        return HttpResponse('PRODUCTO NO HALLADO')
+
+def eliminar_producto(request,id):
+    try:
+        prd_a_eliminar = Products.objects.get(id=id)
+        print(prd_a_eliminar.nombre)
+        print(prd_a_eliminar.id)
+        prd_a_eliminar.delete()
+        context = {
+            'message' : 'PRODUCTO ELIMINADO CORRECTAMENTE'
+        }
+        return render(request,'productos/productos.html',context=context)
+    except:
+        return HttpResponse('EL PRODUCTO NO SE PUDO ELIMINAR')
+
+
+
+        
